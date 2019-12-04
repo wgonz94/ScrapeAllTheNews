529f3f759a069f334eea6e1d0fdcb7eac99b2bcd
William Gonzales <wgonz94@yahoo.com>
2019-12-03 22:05:28 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const db = require("./models");
+
+const PORT = 3000; // go back to project to check port
+
+const app = express();
+
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
